d7a36a1fd976bc1111e513a89899482936577fc0
Pasquale Cautela <pasquale.cautela@dedalus.eu>
2018-02-19 14:18:06 +0100

    form first commit

---
+++ src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-form',
+  templateUrl: './joke-form.component.html',
+  styleUrls: ['./joke-form.component.css']
+})
+export class JokeFormComponent {
+
+  constructor() { }
+
+}
